095cdd5311b5a5ddf5a71537bfdede2b11c40381
SAKSHAM_CHOUHAN <131700345+samchouhan@users.noreply.github.com>
2026-02-12 22:43:15 +0530

    Implement factorial functions using iteration and recursion

---
+++ factorial.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int fact1(int n) // O(n)
+{
+    int fact = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+    return fact;
+}
+int fact2(int n)  // TC O(n)
+{
+    if (n == 1)
+        return 1;
+    return n * fact2(n - 1);
+}
+
+int main()
+{
+    printf("%d\n", fact1(5));
+    printf("%d\n", fact2(5));
